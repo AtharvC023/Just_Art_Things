d73646be3fc2e1682b49b0839d3ac542de1d68e3
AtharvC023 <atharvchaudhari023@gmail.com>
2026-02-19 11:25:43 +0530

    Add vercel.json for proper redirect

--- art-things-brochure/app/page.tsx
+++ art-things-brochure/app/page.tsx
@@ -1,14 +1,3 @@
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-
 export default function RootPage() {
-  const router = useRouter()
-  
-  useEffect(() => {
-    router.push('/landing')
-  }, [router])
-  
   return null
 }
